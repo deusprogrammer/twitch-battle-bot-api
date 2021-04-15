a6730680b28be0ed6798eeee61161d33e6d4ddcb
Michael C. Main <deusprogrammer@gmail.com>
2021-04-15 17:48:44 -0500

    Added update of refresh token...hopefully it works

--- api/routes/bots.js
+++ api/routes/bots.js
@@ -177,7 +177,10 @@ router.route("/:id")
                 // Refresh token on failure to validate
                 let refresh = await refreshAccessToken(bot.refreshToken);
                 bot.accessToken = refresh.access_token;
+                bot.refreshToken = refresh.refresh_token;
                 await Bots.findByIdAndUpdate(bot._id, bot);
+                
+                console.log("REFRESHED TOKEN SUCCESSFULLY");
             }
             
             return response.json(bot);
