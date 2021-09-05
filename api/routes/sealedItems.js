69fe2d44727ba0e18ec8d1a05d6a4dc21d5911ef
Michael Main <deusprogrammer@gmail.com>
2021-09-05 14:47:43 -0500

    Fixed sealed item problem

--- api/routes/sealedItems.js
+++ api/routes/sealedItems.js
@@ -49,7 +49,7 @@ router.route("/:id")
             }
 
             // Null out the code if not the broadcaster or bot
-            if (!authenticatedUserHasAccessToChannel(request, sealedItem.owningChannel) && !authenticatedUserHasRole(request, "TWITCH_BOT")) {
+            if (!authenticatedUserHasAccessToChannel(request, results.owningChannel) && !authenticatedUserHasRole(request, "TWITCH_BOT")) {
                 results.code = null;
             }
 
