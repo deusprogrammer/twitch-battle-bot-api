ed23874c306c9adbaf1a906fe10969628e20247e
Michael Main <deusprogrammer@gmail.com>
2021-10-06 21:43:04 -0500

    Added defaults for raid alerts

--- api/models/bots.js
+++ api/models/bots.js
@@ -72,7 +72,9 @@ var botSchema = new mongoose.Schema({
         type: Map,
         default: {
             "cbd": true,
-            "requests": true
+            "requests": false,
+            "raid": false,
+            "rewards": false
         }
     },
     videoPool: {
