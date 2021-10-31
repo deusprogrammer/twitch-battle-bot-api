f7b1f3b3257cedff3ea1aaecb21af1c1c9a8b824
Michael Main <deusprogrammer@gmail.com>
2021-10-31 17:45:24 -0500

    Added raidTheme

--- api/models/bots.js
+++ api/models/bots.js
@@ -68,6 +68,10 @@ var botSchema = new mongoose.Schema({
         type: Number,
         default: 0
     },
+    raidTheme: {
+        type: String,
+        default: "YOSHI"
+    },
     config: {
         type: Map,
         default: {
