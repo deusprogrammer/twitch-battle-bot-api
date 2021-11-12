6547aacb2c2c03923910638e3b1a17d0ade112f5
Michael Main <deusprogrammer@gmail.com>
2021-11-11 22:13:25 -0600

    Fixed issue?

--- api/routes/users.js
+++ api/routes/users.js
@@ -181,7 +181,7 @@ router.route("/:id")
 
                 try {
                     if (!authenticatedUserHasRole(request, "TWITCH_BOT") /*&& !authenticatedUserHasRole(request, "TWITCH_ADMIN")*/) {
-                        if (["equip", "sell"].includes(type) && !user.inventory.contains(id)) {
+                        if (["equip", "sell"].includes(type) && !user.inventory.includes(id)) {
                             return response.status(400).send();
                         } else if (!["equip", "unequip", "sell"].includes(type)) {
                             return response.status(400).send();
